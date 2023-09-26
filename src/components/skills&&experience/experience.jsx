cb3e34715ced29055d4d6663d3c8489979650eef
sirhmvfx <sirhmvfx@gmail.com>
2023-09-26 17:25:02 +0100

    push

--- src/components/skills&&experience/experience.jsx
+++ src/components/skills&&experience/experience.jsx
@@ -1,6 +1,5 @@
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import { ExpCircle } from "../../assets/Icons";
-import Resume from "../../utils/victor.pdf";
 
 const Experience = () => {
   // const handleDownload = () => {
