faa5a02cdbb927ff015eeb5ec1c201d4f8af62fd
sirhmvfx <sirhmvfx@gmail.com>
2023-09-26 16:43:03 +0100

    cooked

---
+++ src/Data/data.js
@@ -0,0 +1,96 @@
+export const projectData = [
+  {
+    name: "Start Power Accomodations",
+    desc: "converts the text input to an audio output",
+    lang: "JavaScript + Css + Html",
+    link: "https://startpoweraccomodations.netlify.app/",
+  },
+  {
+    name: "Movie App",
+    desc: "A Movie App",
+    lang: " React Js",
+    link: "https://ganiu-samuel-hngx-stage-two.netlify.app/",
+  },
+  {
+    name: "Image Gallery",
+    desc: "A Drag and Drop image gallery",
+    lang: "React Js + FireBase",
+    link: "https://hngx-stage-three-image-gallery.netlify.app/",
+  },
+];
+
+export const experienceData = [
+  {
+    role: "Frontend Developer",
+    span: "2023 - till date",
+    comp: "TekArt",
+    desc: "I work as a Lead Frontend Engineer at Teakrt, crafting thoughtful and inclusive experiences that adhere to web standards.",
+  },
+];
+
+export const skills = [
+  {
+    id: 0,
+    title: "Html",
+  },
+  {
+    id: 1,
+    title: "Css",
+  },
+  {
+    id: 2,
+    title: "Bootstrap",
+  },
+  {
+    id: 3,
+    title: "TailwindCSS",
+  },
+  {
+    id: 4,
+    title: "SCSS",
+  },
+  {
+    id: 5,
+    title: "JavaScript",
+  },
+  {
+    id: 6,
+    title: "TypeScript",
+  },
+  {
+    id: 7,
+    title: "Jquery",
+  },
+  {
+    id: 8,
+    title: "ReactJs",
+  },
+  {
+    id: 9,
+    title: "NextJs",
+  },
+  {
+    id: 10,
+    title: "Git",
+  },
+  {
+    id: 11,
+    title: "Github",
+  },
+  {
+    id: 12,
+    title: "Figma",
+  },
+  {
+    id: 13,
+    title: "Photoshop",
+  },
+  {
+    id: 14,
+    title: "Premiere pro",
+  },
+  {
+    id: 15,
+    title: "...",
+  },
+];
